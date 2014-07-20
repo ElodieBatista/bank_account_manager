629b3003d13d6ec1793b0263034a62f94b2359a0
elodiebatista <elodie.batista@live.fr>
2014-07-20 22:05:22 +0200

    Restructuration

--- app/app/scripts/controllers/CheckingAccountCtrl.js
+++ app/app/scripts/controllers/CheckingAccountCtrl.js
@@ -3,44 +3,44 @@
 var module = angular.module('bamApp');
 
 module.config(function config($routeProvider) {
-    $routeProvider
-        .when('/',
-        {
-            templateUrl: 'views/checkingAccounts.html',
-            controller: 'CheckingAccountCtrl'
-        })
+  $routeProvider
+    .when('/',
+    {
+      templateUrl: 'views/checkingAccounts.html',
+      controller: 'CheckingAccountCtrl'
+    })
 });
 
-module.controller('CheckingAccountCtrl', function CheckingAccountCtrl($scope, accountService, settingsService, categoryService) {
-    accountService.getCheckingAccounts().then(function (checkingAccounts) {
-        $scope.checkingAccounts = checkingAccounts;
-    });
+module.controller('CheckingAccountCtrl', function CheckingAccountCtrl($rootScope, $scope, $resource, accountService, settingsService, categoryService) {
+  accountService.getCheckingAccounts().then(function (checkingAccounts) {
+    $scope.checkingAccounts = checkingAccounts;
+  });
 
-    $scope.getCheckingAccountsForMonth = function (monthNb) {
-        $scope.month = settingsService.getMonths()[parseInt(monthNb) - 1];
-        //$scope.$apply();
-    };
+  $scope.getCheckingAccountsForMonth = function (monthNb) {
+    $scope.month = settingsService.getMonths()[parseInt(monthNb) - 1];
+    //$scope.$apply();
+  };
 
-    $scope.categories = categoryService.getCategories();
+  $scope.categories = categoryService.getCategories();
 
-    $scope.ways = settingsService.getWays();
+  $scope.ways = settingsService.getWays();
 
-    $scope.newTransaction = function(form) {
-        if (form.$valid) {
-            var Auth = $resource($rootScope.srvEndpoint + 'auth/?');
-            var uriParams = {'email': form.email, 'pass': form.pass};
-            Auth.get(uriParams, function(httpResponse) {
-                _onLoginSuccess(form, httpResponse);
-            }, function(httpResponse) {
-                if(httpResponse.data.errorMessage){
-                    $scope.errorMsg = httpResponse.data.errorMessage;
-                } else if(httpResponse.status == 0) {
-                    $scope.errorMsg = "Unable to connect to Duxter. Please check your Internet connection and try again."
-                }
-                tracker.trackEvent("error", "login_failed",
-                    "status: " + JSON.stringify(httpResponse.status) + ", data: " + JSON.stringify(httpResponse.data));
-                _onLoginError();
-            });
+  $scope.newTransaction = function(form) {
+    if (form.$valid) {
+      var Auth = $resource($rootScope.srvEndpoint + 'auth/?');
+      var uriParams = {'email': form.email, 'pass': form.pass};
+      Auth.get(uriParams, function(httpResponse) {
+        _onLoginSuccess(form, httpResponse);
+      }, function(httpResponse) {
+        if(httpResponse.data.errorMessage){
+          $scope.errorMsg = httpResponse.data.errorMessage;
+        } else if(httpResponse.status == 0) {
+          $scope.errorMsg = 'Unable to connect to Duxter. Please check your Internet connection and try again.'
         }
+        tracker.trackEvent('error', 'login_failed',
+            'status: ' + JSON.stringify(httpResponse.status) + ', data: ' + JSON.stringify(httpResponse.data));
+        _onLoginError();
+      });
     }
+  }
 });
