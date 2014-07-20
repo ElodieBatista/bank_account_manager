6032f16a525d5bc0af483ac400bee9ee17ad5279
elodiebatista <elodie.batista@live.fr>
2014-07-20 22:05:26 +0200

    Restructuration

--- app/app/scripts/controllers/HomeCtrl.js
+++ app/app/scripts/controllers/HomeCtrl.js
@@ -1,5 +1,34 @@
 'use strict';
 
-angular.module('bamApp').controller('HomeCtrl', function ($scope) {
+angular.module('bamApp').config(function config($routeProvider) {
+  $routeProvider
+    .when('/',
+    {
+      templateUrl: 'views/home.tpl.html',
+      controller: 'HomeCtrl'
+    })
+}).controller('HomeCtrl', function ($scope, $rootScope, accountService) {
+  $scope.test = 'test';
 
+  $rootScope.currViewMonth = new Date().getMonth() + 1;
+
+  accountService.getCheckingAccountsMonth($rootScope.currViewMonth).then(function(accounts) {
+    $scope.checkingAccounts = accounts;
+  });
+
+
+  $rootScope.changeViewMonth = function(month) {
+    $rootScope.currViewMonth = month;
+  };
+
+  $rootScope.$watch('currViewMonth', function () {
+    $scope.getViewMonth($rootScope.currViewMonth);
+  });
+
+  $scope.getViewMonth = function(month) {
+    $scope.test = month;
+    accountService.getCheckingAccountsMonth(month).then(function(accounts) {
+      $scope.checkingAccounts = accounts;
+    });
+  }
 });
