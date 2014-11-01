fb108dfbc166ddecd04a5bf0318b774cbce89deb
elodiebatista <elodie.batista@live.fr>
2014-11-01 16:36:55 +0100

    Adding more translations on categories

--- app/app/scripts/account/monthlyReport/monthlyReport.js
+++ app/app/scripts/account/monthlyReport/monthlyReport.js
@@ -1,19 +1,63 @@
 'use strict';
 
-angular.module('bamApp').controller('MonthlyReportCtrl', function ($scope, $rootScope, $routeParams, apiService, settingsService) {
+angular.module('bamApp').controller('MonthlyReportCtrl', function ($scope, $rootScope, $routeParams, $q, $translate, apiService, settingsService) {
     $rootScope.currViewMonth = parseInt($routeParams.month);
     var currYear = settingsService.getCurrentYear().name;
 
+    function translateData(data) {
+        var deferred = $q.defer();
+        $translate(data).then(function (translations) {
+            var result = [];
+            for (var prop in translations) {
+                result.push(translations[prop]);
+            }
+            return deferred.resolve(result);
+        });
+        return deferred.promise;
+    }
+
     apiService.ReportSpendingByCategory.get({'year':currYear, 'month':$rootScope.currViewMonth}, function(res) {
-        $scope.spendingByCategory = res.data;
+        var categories = [];
+        for (var i = 0, l = res.data.length; i < l; i++) {
+            categories.push(res.data[i].name);
+        }
+        translateData(categories).then(function(data) {
+            for (var i = 0, l = res.data.length; i < l; i++) {
+                res.data[i].name = data[i];
+            }
+            $scope.spendingByCategory = res.data;
+        });
     });
 
     apiService.ReportIncomingByCategory.get({'year':currYear, 'month':$rootScope.currViewMonth}, function(res) {
-        $scope.incomingByCategory = res.data;
+        var categories = [];
+        for (var i = 0, l = res.data.length; i < l; i++) {
+            categories.push(res.data[i].name);
+        }
+        translateData(categories).then(function(data) {
+            for (var i = 0, l = res.data.length; i < l; i++) {
+                res.data[i].name = data[i];
+            }
+            $scope.incomingByCategory = res.data;
+        });
     });
 
     apiService.ReportTransactionsByCategory.get({'year':currYear, 'month':$rootScope.currViewMonth}, function(res) {
-        $scope.transactionsByCategory = res.data;
+        translateData(res.data.categories).then(function(categories) {
+            var legend = [];
+            for (var i = 0, l = res.data.series.length; i < l; i++) {
+                legend.push(res.data.series[i].name);
+            }
+            translateData(legend).then(function(data) {
+                for (var i = 0, l = res.data.series.length; i < l; i++) {
+                    res.data.series[i].name = data[i];
+                }
+                $scope.transactionsByCategory = {
+                    categories: categories,
+                    series: res.data.series
+                };
+            });
+        });
     });
 
     apiService.ReportTotalByMonth.get({'year':currYear}, function(res) {
